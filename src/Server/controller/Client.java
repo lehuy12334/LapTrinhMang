6eece216732cf204df077a4e88226126089b8a0e
lehuy12334 <72182640+lehuy12334@users.noreply.github.com>
2021-12-13 02:02:19 +0700

    Count lose

--- src/Server/controller/Client.java
+++ src/Server/controller/Client.java
@@ -573,7 +573,7 @@ public class Client implements Runnable {
                         winner.setMatchCount(winner.getMatchCount() +1);
                         //người thua
                         loser.addScore(-2);
-                        loser.setLoseCount(loser.getLoseCount() - 1);
+                        loser.setLoseCount(loser.getLoseCount() + 1);
                         loser.setMatchCount(loser.getMatchCount() +1);
                         bus.update(winner);
                         bus.update(loser);
