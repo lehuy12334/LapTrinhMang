b57b76b96a87b5bf27a9326e580c3852245f2f18
lehuy12334 <72182640+lehuy12334@users.noreply.github.com>
2021-12-13 04:28:14 +0700

    data

--- src/Server/DB/carodb.sql
+++ src/Server/DB/carodb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 12, 2021 at 09:24 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.3.33
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 12, 2021 lúc 10:27 PM
+-- Phiên bản máy phục vụ: 10.4.11-MariaDB
+-- Phiên bản PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `carodb`
+-- Cơ sở dữ liệu: `carodb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gamematch`
+-- Cấu trúc bảng cho bảng `gamematch`
 --
 
 CREATE TABLE `gamematch` (
@@ -39,7 +39,7 @@ CREATE TABLE `gamematch` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `gamematch`
+-- Đang đổ dữ liệu cho bảng `gamematch`
 --
 
 INSERT INTO `gamematch` (`ID`, `PlayerID1`, `PlayerID2`, `WinnerID`, `PlayTime`, `TotalMove`, `StartedTime`, `Chat`) VALUES
@@ -48,12 +48,15 @@ INSERT INTO `gamematch` (`ID`, `PlayerID1`, `PlayerID2`, `WinnerID`, `PlayTime`,
 (3, 3, 4, 4, 30, 35, '2021-11-23T17:22:06.081', NULL),
 (4, 1, 4, 4, 40, 45, '2021-11-23T17:22:06.081', NULL),
 (5, 3, 2, 3, 50, 55, '2021-11-23T17:22:06.081', NULL),
-(6, 4, 5, 5, 90, 50, '2021-11-23T17:22:06.081', NULL);
+(6, 4, 5, 5, 90, 50, '2021-11-23T17:22:06.081', NULL),
+(7, 1, 3, 1, 12, 10, '2021-12-13T03:28:36.415594700', NULL),
+(8, 3, 1, 3, 11, 10, '2021-12-13T03:55:25.981215900', NULL),
+(9, 3, 1, 3, 10, 11, '2021-12-13T04:23:19.010888900', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `player`
+-- Cấu trúc bảng cho bảng `player`
 --
 
 CREATE TABLE `player` (
@@ -74,13 +77,13 @@ CREATE TABLE `player` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `player`
+-- Đang đổ dữ liệu cho bảng `player`
 --
 
 INSERT INTO `player` (`ID`, `Email`, `Password`, `Avatar`, `Name`, `Gender`, `YearOfBirth`, `Score`, `MatchCount`, `WinCount`, `LoseCount`, `CurrentStreak`, `Rank`, `Blocked`) VALUES
-(1, 'huy123@gmail.com', 'b8dc042d8cf7deefb0ec6a264c930b02', 'icons8_trollface_96px.png', 'QuocHuy', 'Nam', 2000, 14, 20, 10, 8, 2, 1, 0),
+(1, 'huy123@gmail.com', 'b8dc042d8cf7deefb0ec6a264c930b02', 'icons8_trollface_96px.png', 'QuocHuy', 'Nam', 2000, 13, 21, 11, 8, 2, 1, 0),
 (2, 'lam123@gmail.com', '740273e48aa999a020df445d4e429517', 'icons8_circled_user_male_skin_type_7_96px.png', 'Lam', 'Nam', 2000, 34, 20, 16, 9, 4, 4, 0),
-(3, 'thanh123@gmail.com', '893c3fd491f30b629fde7abe2ba1b516', 'icons8_circled_user_male_skin_type_7_96px.png', 'Thanh', 'Nam', 2000, 24, 34, 15, 4, 8, 2, 0),
+(3, 'thanh123@gmail.com', '893c3fd491f30b629fde7abe2ba1b516', 'icons8_circled_user_male_skin_type_7_96px.png', 'Thanh', 'Nam', 2000, 28, 35, 17, 3, 8, 2, 0),
 (4, 'van123@gmail.com', 'a0e70be9e8f538282678aecf1ecc1f43', 'icons8_circled_user_male_skin_type_7_96px.png', 'Huy', 'Nam', 2000, 12, 34, 10, 7, 5, 3, 0),
 (5, 'g@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'icons8_circled_user_male_skin_type_7_96px.png', 'nam', 'Nam', 2000, 13, 36, 12, 5, 2, -1, 0),
 (6, 'f@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'icons8_circled_user_male_skin_type_7_96px.png', 'hoa', 'Nữ', 2001, 40, 15, 9, 7, 3, -1, 0),
@@ -93,34 +96,34 @@ INSERT INTO `player` (`ID`, `Email`, `Password`, `Avatar`, `Name`, `Gender`, `Ye
 (13, 'k@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'icons8_circled_user_male_skin_type_7_96px.png', 'lan', 'Nữ', 1999, 17, 25, 14, 6, 5, -1, 0);
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `gamematch`
+-- Chỉ mục cho bảng `gamematch`
 --
 ALTER TABLE `gamematch`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `player`
+-- Chỉ mục cho bảng `player`
 --
 ALTER TABLE `player`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `UNIQUE` (`Email`) USING BTREE;
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `gamematch`
+-- AUTO_INCREMENT cho bảng `gamematch`
 --
 ALTER TABLE `gamematch`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `player`
+-- AUTO_INCREMENT cho bảng `player`
 --
 ALTER TABLE `player`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
